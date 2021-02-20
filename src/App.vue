e72c2776903a2557196926c85d16578fb6ff22c7
cjy324 <juy32400@naver.com>
2021-02-20 19:49:40 +0900

    21.02.20 tailwind 설치완료

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
-  HOME
+  
+  <div class="container mx-auto bg-green-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores beatae dignissimos quidem omnis dolorum totam quae ipsa architecto quibusdam ut, id voluptas. Iure autem totam enim numquam asperiores quos velit!
+  Doloremque blanditiis voluptates, eligendi incidunt mollitia ducimus aspernatur facilis officiis, provident adipisci cumque quae dignissimos soluta. Cupiditate cumque ipsa commodi, porro sint quibusdam dolore assumenda reprehenderit quod suscipit inventore labore.
+  Eum soluta assumenda debitis voluptatibus veniam quo, impedit quas pariatur vero repudiandae quaerat. Veniam obcaecati, ipsam voluptatum odio illo maxime, quisquam aut tempore harum impedit, soluta voluptatem tempora? Soluta, repellendus!
+  Minus voluptatibus commodi iusto neque saepe ab doloribus. Officia voluptatum quibusdam facilis perferendis maxime incidunt fuga, voluptatem nesciunt aspernatur est iste perspiciatis tenetur iusto, sequi exercitationem blanditiis minus corporis. Omnis.
+  </div>
 </template>
 
 <script lang="ts">
